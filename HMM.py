1ffe126df5002c3deb4cdfeff85f4a48ab3766b0
Carmen Carter <cacarter5@dons.usfca.edu>
2024-11-13 18:23:41 -0800

    Adding unit test and command line

--- HMM.py
+++ HMM.py
@@ -1,14 +1,13 @@
-
+## Carmen Carter
+## CS 386
 
 import random
 import argparse
 import codecs
 import os
 import sys
-from turtledemo.penrose import start
 
 import numpy as np
-from jinja2.sandbox import MAX_RANGE
 
 
 # Sequence - represents a sequence of hidden states and corresponding
@@ -41,7 +40,6 @@ class HMM:
     ## part 1 - you do this.
     ## global variable for lander cases
     LANDER = False
-    #TODO: Create unit test for load
     def load(self, basename):
         """reads HMM structure from transition (basename.trans),
         and emission (basename.emit) files,
@@ -96,7 +94,6 @@ class HMM:
             next_weights = []
             for state in next_states:
                 next_weights.append(self.transitions.get(cur_state, {}).get(state))
-
             emis_states.append(np.random.choice(next_emis_states, p=next_emis_weights))
             cur_state = np.random.choice(next_states, p=next_weights)
             trans_states.append(cur_state)
@@ -105,26 +102,29 @@ class HMM:
     ## determine the most likely sequence of states.
     def forward(self, sequence):
         # setup matrix
-
         m = []
         intial = [v for v in self.transitions]
-        seq_row = [""] * 2 + [str(s) for s in sequence.outputseq]
+        #print(intial)
+        seq = sequence.split(' ')
+        seq_row = [""] * 2 + [s.strip() for s in seq]
         m.append(seq_row)
         for i in intial :
             if i == '#':
-                i_row = [i] + [1.0] + [0] * len(sequence.outputseq)
+                i_row = [i] + [1.0] + [0] * len(seq)
             else :
-                i_row = [i] + [0] * (len(sequence.outputseq) + 1)
+                i_row = [i] + [0] * (len(seq) + 1)
             m.append(i_row)
         # compute values for day 1
+
         for k in range(len(intial) - 1) :
-            if self.emissions.get(m[k + 2][0], {}).get(m[0][2]) is not None:
+            if self.emissions.get(m[k + 2][0], {}).get(m[0][2]) is not None and self.transitions.get("#", {}).get(m[k + 2][0]) is not None :
                 m[k + 2][2] = float(self.emissions.get(m[k + 2][0], {}).get(m[0][2])) * float(self.transitions.get("#", {}).get(m[k + 2][0]))
             else :
                 m[k + 2][2] = 0
         # compute values for day 2,...n
+
         intial.remove("#")
-        for j in range(3, len(sequence) + 2) :
+        for j in range(3, len(seq) + 2) :
             state_index = 2
             for state in intial :
                 s_sum = 0
@@ -136,23 +136,26 @@ class HMM:
                     # P(happy) = value at matrix at previous column
 
                     ##Check for none type due to not emissions and values not aligning in all cases
-                    if self.emissions.get(state, {}).get(m[0][j]) is not None :
+                    if self.emissions.get(state, {}).get(m[0][j]) is not None and self.transitions.get(s2, {}).get(state) is not None:
 
                         s_sum += (float(self.emissions.get(state, {}).get(m[0][j])) *
                                      float(self.transitions.get(s2, {}).get(state)) *
                                      float(m[s2_indx][j - 1]))
                     else :
-                        s_sum += 0.0 * float(self.transitions.get(s2, {}).get(state)) * float(m[s2_indx][j - 1])
+                        s_sum += 0.0
+
                     s2_indx += 1
+
                 m[state_index][j] = s_sum
                 state_index += 1
+
         ## determine the most probable state
         #get the value at the last column in each row
-        probable_state_val = m[2][len(sequence.outputseq) + 1]
+        probable_state_val = m[2][len(seq) + 1]
         probable_state_indx = 2
-        for i in range (3, len(sequence.outputseq) + 1) :
-            if m[i][len(sequence.outputseq) + 1] > probable_state_val :
-                probable_state_val = m[i][len(sequence.outputseq) + 1]
+        for i in range (3, len(intial)) :
+            if m[i][len(seq) + 1] > probable_state_val :
+                probable_state_val = m[i][len(seq) + 1]
                 probable_state_indx = i
         probable_state = m[probable_state_indx][0]
 
@@ -171,27 +174,27 @@ class HMM:
         m = []
         ## backpointer matrix
         b = []
-
         intial = [v for v in self.transitions]
-        seq_row = [""] * 2 + [str(s) for s in sequence.outputseq]
+        seq = sequence.split(" ")
+        seq_row = [""] * 2 + [s for s in seq]
         m.append(seq_row)
         b.append(seq_row)
         for i in intial :
             if i == '#':
-                i_row = [i] + [1.0] + [0] * len(sequence.outputseq)
+                i_row = [i] + [1.0] + [0] * len(seq)
             else :
-                i_row = [i] + [0] * (len(sequence.outputseq) + 1)
+                i_row = [i] + [0] * (len(seq) + 1)
             m.append(i_row)
-            b.append([i] + [0] * (len(sequence.outputseq) + 1))
+            b.append([i] + [0] * (len(seq) + 1))
         # compute values for day 1
         for k in range(len(intial) - 2) :
-            if self.emissions.get(m[k + 2][0], {}).get(m[0][2]) is not None:
+            if self.emissions.get(m[k + 2][0], {}).get(m[0][2]) is not None and self.transitions.get("#", {}).get(m[k + 2][0]) is not None:
                 m[k + 2][2] = float(self.emissions.get(m[k + 2][0], {}).get(m[0][2])) * float(self.transitions.get("#", {}).get(m[k + 2][0]))
             else :
                 m[k + 2][2] = 0
         # compute values for day 2..
         intial.remove("#")
-        for j in range(3, len(sequence) + 2) :
+        for j in range(3, len(seq) + 2) :
             state_index = 2
             for state in intial :
                 max_val = 0
@@ -199,12 +202,12 @@ class HMM:
                 max_indx = 0
                 for s2 in intial :
                     # find the value and set the max
-                    if self.emissions.get(state, {}).get(m[0][j]) is not None:
+                    if self.emissions.get(state, {}).get(m[0][j]) is not None and self.transitions.get(s2, {}).get(state) is not None:
                         val = (float(self.emissions.get(state, {}).get(m[0][j])) *
                                      float(self.transitions.get(s2, {}).get(state)) *
                                      float(m[s2_indx][j - 1]))
                     else :
-                        val = 0.0  * float(self.transitions.get(s2, {}).get(state)) * float(m[s2_indx][j - 1])
+                        val = 0.0
 
                     if val > max_val :
                         max_val = val
@@ -215,18 +218,19 @@ class HMM:
                 state_index += 1
 
         # #get the most likely state
-        probable_state_val = m[2][len(sequence.outputseq) + 1]
+        probable_state_val = m[2][len(seq) + 1]
         probable_state_indx = 2
-        for i in range (3, len(sequence.outputseq) + 1) :
-            if m[i][len(sequence.outputseq) + 1] > probable_state_val :
-                probable_state_val = m[i][len(sequence.outputseq) + 1]
+        for i in range (3, len(intial)) :
+            if m[i][len(seq) + 1] > probable_state_val :
+                probable_state_val = m[i][len(seq) + 1]
                 probable_state_indx = i
         probable_state = m[probable_state_indx][0]
         ## traverse back using the backpointers
         state_list = [probable_state]
 
         current_index = probable_state_indx
-        for j in range(len(sequence.outputseq) + 1, 2, -1):
+
+        for j in range(len(seq) + 1, 2, -1):
             current_index = b[current_index][j] + 1
             state_list.append(m[current_index][0])
         #reverse the list
@@ -237,24 +241,32 @@ class HMM:
     ## You do this. Given a sequence with a list of emissions, fill in the most likely
     ## hidden states using the Viterbi algorithm.
 
-
-
-
 if __name__ == '__main__':
-    #TODO: Implement command line arguments
-    #TODO: Finish lander emit and trans files
-    h = HMM()
-    h.load('partofspeech')
-    # print(h.transitions)
-    # print(h.emissions)
 
-    # if len(sys.argv) < 3:
-    #     print("Please provide all arguments")
+    basename = ''
+    func = ''
+    param = ''
 
-    # basename = sys.argv[1]
-    # func = sys.argv[2]
-    # param = sys.argv[3]
+    if len(sys.argv) < 3:
+         print("Please provide all arguments")
+    else :
+        basename = sys.argv[3]
+        func = sys.argv[4]
+        param = sys.argv[5]
 
-    h.viterbi(h.generate(3))
+    hm = HMM()
+    hm.load(basename)
+    if func == '--generate' :
+        print(hm.generate(int(param)))
 
+    if func == '--forward' :
+        r = open(param).readlines()
+        for line in r :
+            if line.strip() :
+                hm.forward(line)
 
+    if func == '--viterbi' :
+        r = open(param).readlines()
+        for line in r :
+            if line.strip() :
+                hm.viterbi(line)
